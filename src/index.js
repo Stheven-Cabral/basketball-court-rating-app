ccd1ba90cba380d5bdeb08101e05faa5d52fc5f2
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-13 16:05:08 -0700

    Update project index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+import {ApolloProvider} from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+//The constructor for ApolloClient accepts an ApolloClientOptions object that supports the required and optional fields listed below. These fields make it easy to customize how Apollo works based on your application's needs. (https://www.apollographql.com/docs/react/api/apollo-client/#gatsby-focus-wrapper)
+const client = new ApolloClient({
+  //Caching is the term for storing reusable responses in order to make subsequent requests faster. ... Subsequent requests for cached content can then be fulfilled from a cache closer to the user instead of sending the request all the way back to the web server.
+  cache: new InMemoryCache(),
+  //By default, Apollo Client uses Apollo Link's HttpLink to send GraphQL operations to a remote server over HTTP. Apollo Client takes care of creating this default link.
+  link: new HttpLink({
+    url: 'https://basketball-court-rating-app.herokuapp.com/console/api-explorer',
+  })
+});
+
+const App = () => {
+  return(
+    <ApolloProvider client={client}>
+      <div>
+        <h2>React basketball court yelp app</h2>
+      </div>
+  </ApolloProvider>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
