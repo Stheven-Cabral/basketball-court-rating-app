76d0489e3b568e50419c2a0510eb50847d736b95
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-15 19:36:04 -0700

    Update styling

--- src/components/shared/Badge.js
+++ src/components/shared/Badge.js
@@ -1,7 +1,14 @@
 import styled from "@emotion/styled";
 
 export const Badge = styled.span`
-  margin: 0px 10px;
+  display: inline-block;
+  margin: 0rem 1rem;
+  padding: 0.25rem 0.75rem;
   background-color: grey;
-  font-size: 0.65rem;
+  font-size: 1rem;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  border-radius: 0.5rem;
+  background-color: maroon;
 `;
