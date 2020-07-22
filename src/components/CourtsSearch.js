1d13ac1fd19093ed8974528fdcb6534eed224a1b
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-21 22:09:46 -0700

    Update CourtsSearch.js

--- src/components/CourtsSearch.js
+++ src/components/CourtsSearch.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import SearchForm from './components/SearchForm';
+import Courts from './components/Courts';
 
 export default class CourtsSearch extends Component {
   state = {
