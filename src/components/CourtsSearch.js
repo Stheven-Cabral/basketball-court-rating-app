ed4d0b6ca7640f62ea5d14acf3f5f55bef90fb8d
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-22 20:50:53 -0700

    Update CourtsSearch.js

--- src/components/CourtsSearch.js
+++ src/components/CourtsSearch.js
@@ -3,7 +3,18 @@ import { useLazyQuery, gql } from '@apollo/client';
 import SearchForm from './SearchForm';
 import Courts from './Courts';
 
-const SEARCH = gql``;
+const SEARCH = gql`
+  query Search ($match: String) {
+    basketball_courts(where: {courtName: {_ilike: $match}}, order_by: {courtName: asc}) {
+      id
+      courtName
+      location
+      environment
+      backboardMaterial
+      rimType
+    }
+  }
+`;
 
 const CourtsSearch = () => {
   const [inputValue, setInputValue] = useState('');
