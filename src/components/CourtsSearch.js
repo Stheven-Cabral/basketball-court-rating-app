ad7d36df80e937da66d7f2ab31c05b75123d9fa9
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-22 19:55:17 -0700

    Update CourtsSearch.js

--- src/components/CourtsSearch.js
+++ src/components/CourtsSearch.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import SearchForm from './SearchForm';
 import Courts from './Courts';
 
+const SEARCH = gql``;
+
 export default class CourtsSearch extends Component {
   state = {
     inputValue: ''
