293edb74b17fc3504125411cc6171ffb45ab3d34
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-03 15:50:30 -0700

    Update Court.js

--- src/components/Court.js
+++ src/components/Court.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {userQuery, gql} from '@apollo/client';
+import {List, ListItem} from './shared/List';
+import {Badge} from './shared/Badge';
+
+const Court = gql``;
 
 const Court = () => {
 
