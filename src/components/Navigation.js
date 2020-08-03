bf5daeda62e80fed6db26b4da085a7269d5e663f
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-03 16:51:47 -0700

    Review page and home link update.
    - Update the review page.
    - Allow the "HoopUp" logo to be clickable in order to navigate home.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const NavBar = styled.div`
   height: 70px;
   background-color: maroon;
 `;
 
-const NavLogo = styled.h3`
-  display: inline-block;
-  line-height: 70px;
-  margin-block-start: 0px;
-  margin-block-end: 0px;
-  margin: 0px 10px;
-  vertical-align: middle;
-  color: white;
-  letter-spacing: 2px;
-`;
-
 const Navigation = () => {
   return (
     <NavBar>
-      <NavLogo>HoopUp</NavLogo>
+      <Link className="navlogo" to="/">HoopUp</Link>
     </NavBar>
   )
 }
