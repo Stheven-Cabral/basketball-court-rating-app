063c2202ae90836dc6746f3e745871e0ec98e4e2
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-06 15:32:24 -0700

    Create ReviewForm.js

---
+++ src/components/shared/ReviewForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "@emotion/styled";
+import { Input, Button } from './Form';
+
+export const container = styled.div`
+  display: flex;
+  align-items: center;
+  > button {
+    margin-left: 1rem;
+  }
+`;
+
