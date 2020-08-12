d1d7186bcbbba68cd18c2447a0f03bbf9cef98c0
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-12 00:29:09 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,10 +7,24 @@ const NavBar = styled.div`
   background-color: maroon;
 `;
 
+const SignUp = styled.span`
+  color: lightgrey;
+  float: right;
+  margin-right: 15px;
+  margin-left: 25px;
+`;
+
+const Login = styled.span`
+  color: lightgrey;
+  float: right;
+`;
+
 const Navigation = () => {
   return (
     <NavBar>
       <Link className="navlogo" to="/">HoopUp</Link>
+      <SignUp>Sign Up</SignUp>
+      <Login>Login In</Login>
     </NavBar>
   )
 }
